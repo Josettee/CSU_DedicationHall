6f66bebcb9f9faa1577d3ffdb4c9e11b729d2b5d
youyiBYSKY <2576729985@qq.com>
2023-04-10 21:57:20 +0800

    签名界面完善

--- DedicationHall/miniprogram/pages/Sign_Page/Sign_Page.js
+++ DedicationHall/miniprogram/pages/Sign_Page/Sign_Page.js
@@ -7,26 +7,36 @@ Page({
     context: null,
     index: 0,
     height: 0,
-    width: 0
+    width: 0,
+    ifhidden: '',
   },
   /**记录开始点 */
   bindtouchstart: function(e) {
-    this.data.context.moveTo(e.changedTouches[0].x, e.changedTouches[0].y)
+    this.data.context.moveTo(e.changedTouches[0].x, e.changedTouches[0].y);
+    this.setData({
+      ifhidden:'hidden'
+    })
   },
   /**记录移动点，刷新绘制 */
   bindtouchmove: function(e) {
-   
+    this.data.context.setStrokeStyle('#000000');
     this.data.context.lineTo(e.changedTouches[0].x, e.changedTouches[0].y);
     this.data.context.stroke();
     this.data.context.draw(true);
     this.data.context.moveTo(e.changedTouches[0].x, e.changedTouches[0].y);
   },
   
+
+  bindtouchend: function(e) {
+    this.setData({
+      ifhidden:''
+    })
+  },
   /**清空画布 */
   clear: function() {
     this.data.context.clearRect(0, 0, this.data.width, this.data.height);
     this.data.context.draw();
-    this.data.context.setStrokeStyle('#00ff00')
+    this.data.context.setStrokeStyle('#000000')
     this.data.context.setLineWidth(2)
     this.data.context.setFontSize(20)
     let str = "签名区域";
@@ -34,57 +44,6 @@ Page({
     this.data.context.draw()
   },
   /**导出图片 */
-  export: function() {
-    const that=this;
-    this.data.context.draw(false, wx.canvasToTempFilePath({
-      x: 0,
-      y: 0,
-      width: that.data.width,
-      height: that.data.height,
-      destWidth: that.data.width,
-      destHeight: that.data.height,
-      fileType: 'jpg',
-      canvasId: 'firstCanvas',
-      success(res) {
-        wx.uploadFile({
-          url: 'http://192.168.0.157:8089/uppic', 
-          filePath: res.tempFilePath,
-          name: 'file',
-          success(res) {
-            var url = "http://192.168.0.157:8089/pic?name=" + res.data
-            wx.previewImage({
-              // current: url, // 当前显示图片的http链接
-              urls: [url], // 需要预览的图片http链接列表
-              fail(f) {
-                console.log(2)
-                console.error(f)
-              },
-              success(s) {
-                console.log(1)
-                console.log(s)
-              }
-            })
-          },
-          fail(err) {
-            wx.showToast({
-              title: '上传失败',
-              icon: 'none',
-              duration: 2000
-            })
-          }
-        })
- 
-      },
-      fail() {
-        wx.showToast({
-          title: '导出失败',
-          icon: 'none',
-          duration: 2000
-        })
-      }
-    }))
- 
-  },
   onLoad: function() {
     this.setData({
       width : wx.getSystemInfoSync().screenWidth,
